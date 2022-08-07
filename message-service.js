d35cb8ef7c1b5fdc6b2b95fef0233ffec0dd1640
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-06 22:53:36 -0400

    finished 7.0

--- message-service.js
+++ message-service.js
@@ -2,9 +2,10 @@ export default class MessageService {
     getAllMessages() {
         const request = new XMLHttpRequest();
 
-        new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
             // Setup our listener to process compeleted requests
-            request.onload = function() {
+            request.onload = function () {
+                // Process the response
                 if (request.status >= 200 && request.status < 300) {
                     // If successful
                     const threads = JSON.parse(request.responseText);
@@ -22,16 +23,30 @@ export default class MessageService {
             request.open("GET", "http://zipcode.rocks:8085/messages");
 
             request.send();
-        }).then(successCallback, errorCallback);
+        })
+    }
 
-        function successCallback() {
-            // This data comes from the resolve method
-            console.log("Promise is successful!");
-        }
+    createNewMessage(message) {
+        const request = new XMLHttpRequest();
 
-        function errorCallback() {
-            // This data comes from the reject method
-            console.log("An error occurred");
-        }
+        return new Promise(function (resolve, reject) {
+            // Setup our listener to process compeleted requests
+            request.onload = function () {
+                // Process the response
+                if (request.status >= 200 && request.status < 300) {
+                    // If successful
+                    resolve(JSON.parse(request.responseText));
+                } else {
+                    reject({
+                        status: request.status,
+                        statusText: request.statusText
+                    });
+                }
+            };
+
+            request.open("POST", "http://zipcode.rocks:8085/ids/${message.fromid}/messages");
+
+            request.send(JSON.stringify(message));
+        });
     }
 }
