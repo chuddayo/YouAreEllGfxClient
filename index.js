7fcd981b673cfd44181fa0ab5b583ad08346ac73
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-06 23:19:10 -0400

    passing id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import MessageService from "./message-service.js";
 
 let userId = "chuddayo";
-const messageService = new MessageService();
+const messageService = new MessageService(userId);
 
 window.addEventListener("load", function () {
     document.getElementById("greeting").innerHTML = `Welcome ${userId}!`;
