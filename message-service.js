a9929ee15a8b0f510f14949fde30b2995a443e7d
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-06 21:59:26 -0400

    4.0 done

--- message-service.js
+++ message-service.js
@@ -1,18 +1,37 @@
 export default class MessageService {
     getAllMessages() {
-        let request = new XMLHttpRequest();
+        const request = new XMLHttpRequest();
 
-        // Setup our listener to process compeleted requests
-        request.onload = function() {
-            if (request.status >= 200 && request.status < 300) {
-                console.log(JSON.parse(request.responseText)); // 'This is the returned text.'
-            } else {
-                console.log('Error: ' + request.status); // An error occurred during the request.
-            }
-        };
+        new Promise(function (resolve, reject) {
+            // Setup our listener to process compeleted requests
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 300) {
+                    // If successful
+                    const threads = JSON.parse(request.responseText);
+                    // this data is passed to the success callback
+                    resolve(threads);
+                } else {
+                    // this data is passed to the failure callback
+                    reject({
+                        status: request.status,
+                        statusText: request.statusText
+                    });
+                }
+            };
 
-        request.open("GET", "http://zipcode.rocks:8085/messages");
+            request.open("GET", "http://zipcode.rocks:8085/messages");
 
-        request.send();
+            request.send();
+        }).then(successCallback, errorCallback);
+
+        function successCallback() {
+            // This data comes from the resolve method
+            console.log("Promise is successful!");
+        }
+
+        function errorCallback() {
+            // This data comes from the reject method
+            console.log("An error occurred");
+        }
     }
 }
